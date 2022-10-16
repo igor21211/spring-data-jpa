6bbd6e74316838ced07605849b7a27a43591a5a0
Igor <spuraigor@gmail.com>
2022-10-16 15:12:59 +0300

    Jpa-Work

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,39 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student("Maria2", "Jones", "nadya2.jones@gmail.com", 25);
+            Student ahmed = new Student("Igor", "Ali", "igor22.jones@gmail.com", 32);
+
+
+//            studentRepository.findStudentsByFirstNameEqualsAndAgeEquals(
+//                    "Maria",
+//                    21).forEach(System.out::println);
+
+            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThan(
+                    "Igor",
+                    18).forEach(System.out::println);
+
+            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanNative(
+                    "Igor",
+                    18).forEach(System.out::println);
+            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanExp2(
+                    "Igor",
+                    18).forEach(System.out::println);
+
+            int result = studentRepository.deleteStudentById(15L);
+            System.out.println(result);
+
+            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanExp2(
+                    "Igor",
+                    18).forEach(System.out::println);
+
+
+        };
+    }
+
 }
+
+
